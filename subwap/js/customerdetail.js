a3101b5dc247708249e7270eea3240780bf24abb
djh275220652 <275220652@qq.com>
2017-09-19 17:42:52 +0800

    111

--- subwap/js/customerdetail.js
+++ subwap/js/customerdetail.js
@@ -1,3 +1,9 @@
+var hid='';
+$(document).ready(function(){
+	if(GetQueryString('id')!=''&&GetQueryString('id')!=undefined){
+		hid=GetQueryString('id');
+	}
+});
 var capp=angular.module("cApp",[]);
 capp.controller("customerCtrl",function($scope,$http) {
 	$http({
@@ -28,4 +34,10 @@ function processTwo(obj){
 		$(obj).removeClass('processli-active');
 		$(obj).addClass('processli-active');
 	}
+}
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(decodeURI(r[2]));
+    return null;
 }
