5b0baeb75d20049c5af6139a2f1ed66503d13a58
ztenst <754710007@qq.com>
2017-09-20 22:04:39 +0800

    youhue

--- subwap/js/detail.js
+++ subwap/js/detail.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
             $('.detail-buttom0').css('display','none');
             $('.detail-buttom1').css('display','block');
         }
-        if (detail.is_contact_only==2 || is_user==false){
+        else if (detail.is_contact_only==2 || is_user==false){
             $('.detail-buttom0').css('display','none');
             $('.detail-buttom2').css('display','block');
         }
@@ -177,9 +177,9 @@ function sameArea(){
             }
             if (hid!=samearea[i].id) {
             if(samearea[i].price!=''&&samearea[i].price!=undefined)
-              {$('#samearea ul').append('<li onclick="turnDetail('+samearea[i].id+')"><a href="'+samearea[i].image+'"><div class="detail-mainstyle-img"><img style="width: 7.307rem;height: 5.547rem;" src="'+samearea[i].image+'"></div></a><div class="detail-mainstyle-style">'+samearea[i].title+'</div><div class="detail-mainstyle-area">'+samearea[i].wylx+'</div><div class="detail-samearea-price">'+samearea[i].price+samearea[i].unit+'</div></li>');}
+              {$('#samearea ul').append('<li onclick="turnDetail('+samearea[i].id+')"><div class="detail-mainstyle-img"><img style="width: 7.307rem;height: 5.547rem;" src="'+samearea[i].image+'"></div><div class="detail-mainstyle-style">'+samearea[i].title+'</div><div class="detail-mainstyle-area">'+samearea[i].wylx+'</div><div class="detail-samearea-price">'+samearea[i].price+samearea[i].unit+'</div></li>');}
             else
-                {$('#samearea ul').append('<li onclick="turnDetail('+samearea[i].id+')"><a href="'+samearea[i].image+'"><div class="detail-mainstyle-img"><img style="width: 7.307rem;height: 5.547rem;" src="'+samearea[i].image+'"></div></a><div class="detail-mainstyle-style">'+samearea[i].title+'</div><div class="detail-mainstyle-area">'+samearea[i].wylx+'</div><div class="detail-mainstyle-room"></div></li>');}
+                {$('#samearea ul').append('<li onclick="turnDetail('+samearea[i].id+')"><div class="detail-mainstyle-img"><img style="width: 7.307rem;height: 5.547rem;" src="'+samearea[i].image+'"></div><div class="detail-mainstyle-style">'+samearea[i].title+'</div><div class="detail-mainstyle-area">'+samearea[i].wylx+'</div><div class="detail-mainstyle-room"></div></li>');}
             }
         }
     }else{
