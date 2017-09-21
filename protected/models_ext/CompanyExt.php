9483ad10b063f2425de8bd89aaee68dc438aa345
ztenst <754710007@qq.com>
2017-09-21 17:33:18 +0800

    yopuhua

--- protected/models_ext/CompanyExt.php
+++ protected/models_ext/CompanyExt.php
@@ -16,7 +16,7 @@ class CompanyExt extends Company{
     {
         return array(
             'users'=>array(self::HAS_MANY, 'UserExt', 'cid','condition'=>'users.deleted=0 and users.status=1'),
-            'managers'=>array(self::HAS_MANY, 'UserExt', 'cid','condition'=>'users.deleted=0 and users.status=1 and users.is_manage=1'),
+            'managers'=>array(self::HAS_MANY, 'UserExt', 'cid','condition'=>'managers.deleted=0 and managers.status=1 and managers.is_manage=1'),
         );
     }
 
