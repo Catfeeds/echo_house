5d7099ab5385e699f817fb72835487d98a71a863
djh275220652 <275220652@qq.com>
2017-09-22 18:55:22 +0800

    111

--- subwap/js/personaladd.js
+++ subwap/js/personaladd.js
@@ -1,9 +1,23 @@
+var tags='';
 $(document).ready(function() {
      //validata
     $('#form').validate();
     $.get('/api/tag/publishtags',function(data) {
-        $('.register-attention-text').html(data.data.regis_words);
-        our_uids = data.data.our_uids;
-        kf_id = data.data.kf_id;
+        tags=data.data;
+        for(var i=0;i<tags[0].length;i++){
+          $('#wylx').append('<li><input class="box" type="checkbox" name="住宅"><div class="box-text">住宅</div></li>');
+        }
+        for(var i=0;i<tags[1].length;i++){
+          $('#zxzt').append('<li><input class="box" type="checkbox" name="住宅"><div class="box-text">住宅</div></li>');
+        }
+        for(var i=0;i<tags[2].length;i++){
+          $('#area1').append('<option value="volvo">Volvo</option>');
+        }
+        for(var i=0;i<tags[3].length;i++){
+          $('#mode').append('<li><input class="box" type="radio" name="住宅"><div class="box-text">住宅</div></li>');
+        }
     }); 
 });
+function sub(){
+  
+}
