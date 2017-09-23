b5de4013b88f688b751c369d9e69bb6769729cef
ztenst <754710007@qq.com>
2017-09-23 12:55:44 +0800

    youhua

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -96,7 +96,7 @@ class IndexController extends ApiController
     {
         $this->showUser();
         if(!empty($_COOKIE['qf_uid'])) {
-            if(!empty($_COOKIE['phone'])) {
+            if(empty($_COOKIE['phone'])) {
                 $this->returnError('请绑定经纪圈手机号');
             } else
                 $this->frame['data'] = ['uid'=>$_COOKIE['qf_uid'],'phone'=>$_COOKIE['phone']];
