e61ed26f7dfb80d78f67722a3200d7b37be1e977
ztenst <754710007@qq.com>
2017-09-24 16:45:23 +0800

    youhua

--- protected/models_ext/PlotExt.php
+++ protected/models_ext/PlotExt.php
@@ -304,10 +304,12 @@ class PlotExt extends Plot{
     public function changeS()
     {
         if($owner = $this->owner) {
-            $owner->qf_uid && Yii::app()->controller->sendNotice('恭喜您，'.$this->title.'已通过审核，点击'.Yii::app()->request->getHostInfo().'/subwap/duijieren.html?hid='.$this->id.' 进行付费，完成后房源即可自动展现到新房通前台。
+            $owner->qf_uid && Yii::app()->controller->sendNotice('恭喜您，'.$this->title.'已通过审核并已上线，但是目前没有联系方式。请点击'.Yii::app()->request->getHostInfo().'/subwap/duijieren.html?hid='.$this->id.' 进行付费，完成后您的联系方式将会自动显示到联系人电话列表首位。
 您是'.$this->title.'的发布人可享有以下特权：
 1.新增楼盘最新动态  
 2.您的联系方式始终在电话列表页面、在线申请签约页面、快速报备页面首位。',$owner->qf_uid);
+            SmsExt::sendMsg('项目通过审核',$owner->phone,['lpmc'=>$this->title]);
+            // 恭喜您，${lpmc}已通过后台编辑的完善和审核，请登录经纪圈APP消息列表查看付费链接。
         }
     }
 }
