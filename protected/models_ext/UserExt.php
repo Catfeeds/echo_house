f58ead20076261ff54d13f0efdcc7f117b597faa
ztenst <754710007@qq.com>
2017-09-28 14:18:51 +0800

    youhua

--- protected/models_ext/UserExt.php
+++ protected/models_ext/UserExt.php
@@ -84,10 +84,10 @@ class UserExt extends User{
         }
         else {
             $this->updated = time();
-            if($this->status==1 && $this->qf_uid && ((Yii::app()->db->createCommand('select status from user where qf_uid='.$this->qf_uid)->queryScalar())==0)) {
+            if($this->type==3 && $this->status==1 && $this->qf_uid && ((Yii::app()->db->createCommand('select status from user where qf_uid='.$this->qf_uid)->queryScalar())==0)) {
                 $res = Yii::app()->controller->sendNotice('您的新房通账号已通过审核，欢迎访问经纪圈新房通',$this->qf_uid);
                 // var_dump(SmsExt::sendMsg('经纪人注册通过',$this->phone););exit;
-                $this->status==3 && SmsExt::sendMsg('经纪人注册通过',$this->phone);
+                SmsExt::sendMsg('经纪人注册通过',$this->phone);
                 // HttpHelper::get('http://fang.jj58.com.cn/api/index/sendNotice?uid='.$this->qf_uid.'&words=您的账号已通过审核，欢迎访问经纪圈新房通');
             }
         }
