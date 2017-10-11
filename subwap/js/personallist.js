f064b5d1d7bb3f93d82b2f3d821de7dffd836b8a
ztenst <754710007@qq.com>
2017-10-11 11:37:12 +0800

    youhua

--- subwap/js/personallist.js
+++ subwap/js/personallist.js
@@ -1,39 +1,42 @@
 var kw='';
 var status='';
 var myapp=angular.module("perlist",[]);
-$(document).ready(function() {
-    $.get('/api/index/getQfUid', function(data) {
-        myapp.controller('perlistCtrl',function($scope,$http) {
-            $http({
-                method: 'GET',
-                url: '/api/plot/list?uid=1'
-            }).then(function successCallback(response) {
-                    if (response.data.status=='error') {
-                        alert(response.data.msg);
-                        location.href='list.html';
-                    }else{
-                        $scope.houselist = response.data.data.list;
-                        $('.count').find('b').html(response.data.data.num); 
-                    }
-                }, function errorCallback(response) {
+myapp.controller('perlistCtrl',function($scope,$http) {
+    $http({
+        method: 'GET',
+        url: '/api/index/getQfUid'
+    }).then(function successCallback(response) {
+        $http({
+            method: 'GET',
+            url: '/api/plot/list?uid=1'
+        }).then(function successCallback(response) {
+                if (response.data.status=='error') {
+                    alert(response.data.msg);
+                    location.href='list.html';
+                }else{
+                    $scope.houselist = response.data.data.list;
+                    $('.count').find('b').html(response.data.data.num); 
+                }
+            }, function errorCallback(response) {
 
-            });
-            $scope.search=function() {
-                $('.count').find('b').html('0'); 
-                kw=$('.list-search-frame-text').val();
-                status=$('.statusselect').val();
-                $http({
-                    method: 'GET',
-                    url: '/api/plot/list?uid=1?kw='+kw+'&status='+status,
-                }).then(function successCallback(response) {    
-                        $scope.houselist = response.data.data.list;
-                        $('.count').find('b').html(response.data.data.num); 
-                    }, function errorCallback(response) {
-
-                });
-            }
-                
         });
+        }, function errorCallback(response) {
+
     });
-});
+	
+    $scope.search=function() {
+        $('.count').find('b').html('0'); 
+        kw=$('.list-search-frame-text').val();
+        status=$('.statusselect').val();
+        $http({
+            method: 'GET',
+            url: '/api/plot/list?uid=1?kw='+kw+'&status='+status,
+        }).then(function successCallback(response) {    
+                $scope.houselist = response.data.data.list;
+                $('.count').find('b').html(response.data.data.num); 
+            }, function errorCallback(response) {
+
+        });
+    }
         
+});
