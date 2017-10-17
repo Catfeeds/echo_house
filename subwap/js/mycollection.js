bc0c450c4da6e029f4065986e08d8d746fa6b2f3
djh275220652 <275220652@qq.com>
2017-10-17 17:57:24 +0800

    aaa

--- subwap/js/mycollection.js
+++ subwap/js/mycollection.js
@@ -1,11 +1,52 @@
-$('#collect').click(function() {
-	if ($('#collect').hasClass('save')) {
-		$('#collect').removeClass('save');
-		$('#collect').addClass('notsave');
-		$('#collect').attr('src','./img/notsave.png');
-	} else {
-		$('#collect').removeClass('notsave');
-		$('#collect').addClass('save');
-		$('#collect').attr('src','./img/save.png');
+var hid='';
+var app=angular.module('mycollection',[]);
+app.controller('listCtrl',function($scope,$http){
+//获取列表
+	$http({
+		method:'GET',
+		url:'/api/plot/list?save=1'
+	}).then(function successCallback(response){
+		$scope.list=response.data.data.list;
+		if (response.data.data!='') {
+			$scope.num=response.data.data.list.length;
+		} else {
+			$scope.num=0;
+		}	
+	},function errorCallback(response){
+
+	});
+//搜索
+	$scope.search=function(){
+		var search=$('.list-search-frame-text').val();
+		$http({
+			method:'GET',
+			url:'/api/plot/checkIsZc?kw='+search,
+		}).then(function successCallback(response){
+			if(response.data.status=='success'){
+				if (response.data.data!='') {
+					$scope.num=response.data.data.list.length;
+				} else {
+					$scope.num=0;
+				}
+				$scope.list=response.data.data.list;	
+			}
+			$('.list-search-frame-text').val('');
+			$('.list-search-frame-text').attr('placeholder',search?search:"请输入项目名称");
+		},function errorCallback(response){
+			
+		});
+	}
+//取消收藏
+	$scope.notsave=function($event){	
+		var r=confirm("确定取消收藏吗？");
+		if(r==true){
+			$($event.target).parent().remove();
+			hid=$($event.target).attr('data-id');
+			console.log(hid);
+			$scope.num--;
+			$.get("/api/plot/addSave?hid="+hid,function(data,status){
+	            alert(data.msg);      
+			});
+		}
 	}
 });
