43316df39229ddbb50bb26fbd5c9aae93c73098e
ztenst <754710007@qq.com>
2017-11-01 08:39:20 +0800

    youhua

--- themes/v2/views/wap/my/index.php
+++ themes/v2/views/wap/my/index.php
@@ -89,6 +89,23 @@
         </a>
     </div>
     <script>
+    $(document).ready(function() {
+        <?php if(!($this->staff)):?>
+        if(isWeiXin()) {
+                alert('请下载经纪圈APP查看项目详情');
+                location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.zj58.forum';
+            }else
+                alert('登录成功后请关闭本页面重新进入');
+            QFH5.jumpLogin(function(state,data){
+              //未登陆状态跳登陆会刷新页面，无回调
+              //已登陆状态跳登陆会回调通知已登录
+              //用户取消登陆无回调
+              if(state==2){
+                  alert("您已登陆");
+              }
+          })
+        <?php endif;?>
+    });
         function tocs() {
             <?php if($this->staff):?>
                 <?php if($this->staff->type==1):?>
