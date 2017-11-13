19f7ec61c1d6edb8a607d1e5cfc4ec44e330f6a0
ztenst <754710007@qq.com>
2017-11-13 20:53:47 +0800

    youhua

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -389,6 +389,7 @@ class IndexController extends ApiController
         $appid='wx19afb796cba063c0';$apps='6fd6b825a0095a5bd3edc8cae562b1e2';
         $res = HttpHelper::get("https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$apps&js_code=$code&grant_type=authorization_code");
         if($res){
+            var_dump($res['content']);exit;
             return $res['content']['session_key'];
         }
 
