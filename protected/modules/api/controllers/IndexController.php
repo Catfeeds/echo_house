1b497eb11f6e694a933733c9527cc20a93b2fb20
ztenst <754710007@qq.com>
2017-11-28 14:51:45 +0800

    youhua

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -371,15 +371,15 @@ class IndexController extends ApiController
                         if($noticeuid && $user->qf_uid) {
                             Yii::app()->controller->sendNotice(
                                 '报备项目：'.$plot->title.'
-    客户姓名：'.$tmp['name'].'
-    客户电话： '.$tmp['phone'].'
-    公司门店：'.($user->cid?CompanyExt::model()->findByPk($user->cid)->name:'独立经纪人').'
-    业务员姓名：'.$user->name.'
-    业务员电话：'.$user->phone.'
-    市场对接人：'.$noticename.'
-    对接人电话：'.$notice.'
-    带看时间：'.$_POST['time'].'
-    来访方式：'.($obj->visit_way==1?'自驾':'班车'),$noticeuid);
+客户姓名：'.$tmp['name'].'
+客户电话： '.$tmp['phone'].'
+公司门店：'.($user->cid?CompanyExt::model()->findByPk($user->cid)->name:'独立经纪人').'
+业务员姓名：'.$user->name.'
+业务员电话：'.$user->phone.'
+市场对接人：'.$noticename.'
+对接人电话：'.$notice.'
+带看时间：'.$_POST['time'].'
+来访方式：'.($obj->visit_way==1?'自驾':'班车'),$noticeuid);
                         }
                             
 
