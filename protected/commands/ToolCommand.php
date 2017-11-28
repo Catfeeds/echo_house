a3821b8c5f3c0a47a5cebcacf57b9b469a9807ed
ztenst <754710007@qq.com>
2017-11-28 11:05:44 +0800

    youhua

--- protected/commands/ToolCommand.php
+++ protected/commands/ToolCommand.php
@@ -71,7 +71,7 @@ class ToolCommand extends CConsoleCommand
             $user = $value->user;
             if($user&&$user->qf_uid) {
                 if($p = $value->plot) {
-                    
+                    SmsExt::sendMsg('到期通知',$user->phone,['pro'=>$p->title]);
                     Yii::app()->controller->sendNotice('您的项目'.$p->title.'即将到期，请点击下面链接成为会员，成为会员后您的号码将继续展现，并且可以无限次数发布项目。 http://house.jj58.com.cn/api/index/vip',$user->qf_uid);
                 }
             }
