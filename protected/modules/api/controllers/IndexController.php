6c50d67f65f072616dac968d5bc0fabb6861545d
ztenst <754710007@qq.com>
2018-02-10 16:36:18 +0800

    yohua

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -481,6 +481,7 @@ class IndexController extends ApiController
                             'type'=>$user->type,
                             'is_true'=>$user->is_true,
                             'company_name'=>$user->companyinfo?$user->companyinfo->name:'独立经纪人',
+                            'openid'=>$openid,
                         ];
                         echo json_encode($data);
                     } else {
@@ -557,15 +558,172 @@ class IndexController extends ApiController
             $this->frame['data'] = date('Y-m-d',$user->vip_expire);
     }
 
-    public function actionPayFields()
+    public function actionSetPay($price='',$openid='')
     {
-        $this->frame['data'] = [
-            'appid'=>SiteExt::getAttr('qjpz','appid'),
+        $appid = 'wxc4b995f8ee3ef609';
+        $apps = '48d79f6b24890a88ef5b53a5e5119f5a';
+        
+        // $appid=SiteExt::getAttr('qjpz','appid');
+        $mch_id=1439540602;
+        $body='经纪圈新房通会员支付';
+        $out_trade_no='jjq'.time();
+        $nonce_str=$this->createNoncestr();
+        $notify_url=Yii::app()->request->getHostInfo().'/api/index/pay';
+        $spbill_create_ip = $_SERVER["REMOTE_ADDR"];
+
+        // $stringA="appid=$appid&body=$body&mch_id=$mch_id&nonce_str=$nonce_str&notify_url=$notify_url&out_trade_no=$out_trade_no&spbill_create_ip=$spbill_create_ip&trade_type=JSAPI&total_fee=$price";
+        // $stringSignTemp=$stringA+$apps;
+        // $sign=strtoupper(MD5($stringSignTemp));
+
+        $data = [
+            'appid'=>$appid,
             'mch_id'=>1439540602,
             'body'=>'经纪圈新房通会员支付',
-            'out_trade_no'=>'jjq'.time(),
-            'notify_url'=>Yii::app()->request->getHostInfo().'/api/index/pay',
+            'out_trade_no'=>$nonce_str,
+            'nonce_str'=>$notify_url,
+            // 'sign'=>$sign,
+            'total_fee'=>$price,
+            'spbill_create_ip'=>$spbill_create_ip,
+            'trade_type'=>'JSAPI',
+            'notify_url'=>$notify_url,
+            'openid'=>$openid,
         ];
+        $data['sign'] = $this->getSign($data);
+        // var_dump($data['sign']);;exit;
+         // $this->frame['data'] = $dataxml;
+        // $res = $this->post('https://api.mch.weixin.qq.com/pay/unifiedorder',$dataxml);
+        $xmlData = $this->arrayToXml($data);
+        $return = $this->xmlToArray($this->postXmlCurl($xmlData, 'https://api.mch.weixin.qq.com/pay/unifiedorder', 60));
+        var_dump($return);;exit;
+        
+    }
+
+    //作用：生成签名
+    private function getSign($Obj) {
+        foreach ($Obj as $k => $v) {
+            $Parameters[$k] = $v;
+        }
+        //签名步骤一：按字典序排序参数
+        ksort($Parameters);
+        $String = $this->formatBizQueryParaMap($Parameters, false);
+        //签名步骤二：在string后加入KEY
+        $String = $String . "&key=48d79f6b24890a88ef5b53a5e5119f5a";
+        //签名步骤三：MD5加密
+        $String = md5($String);
+        //签名步骤四：所有字符转为大写
+        $result_ = strtoupper($String);
+        return $result_;
+    }
+
+        ///作用：格式化参数，签名过程需要使用
+    private function formatBizQueryParaMap($paraMap, $urlencode) {
+        $buff = "";
+        ksort($paraMap);
+        foreach ($paraMap as $k => $v) {
+            if ($urlencode) {
+                $v = urlencode($v);
+            }
+            $buff .= $k . "=" . $v . "&";
+        }
+        $reqPar;
+        if (strlen($buff) > 0) {
+            $reqPar = substr($buff, 0, strlen($buff) - 1);
+        }
+        return $reqPar;
+    }
+
+    //微信小程序接口
+    public function weixinapp() {
+        //统一下单接口
+        $unifiedorder = $this->unifiedorder();
+//        print_r($unifiedorder);
+        $parameters = array(
+            'appId' => $this->appid, //小程序ID
+            'timeStamp' => '' . time() . '', //时间戳
+            'nonceStr' => $this->createNoncestr(), //随机串
+            'package' => 'prepay_id=' . $unifiedorder['prepay_id'], //数据包
+            'signType' => 'MD5'//签名方式
+        );
+        //签名
+        $parameters['paySign'] = $this->getSign($parameters);
+        return $parameters;
+    }
+
+        //数组转换成xml
+    private function arrayToXml($arr) {
+        $xml = "<root>";
+        foreach ($arr as $key => $val) {
+            if (is_array($val)) {
+                $xml .= "<" . $key . ">" . arrayToXml($val) . "</" . $key . ">";
+            } else {
+                $xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+            }
+        }
+        $xml .= "</root>";
+        return $xml;
+    }
+
+    //xml转换成数组
+    private function xmlToArray($xml) {
+
+
+        //禁止引用外部xml实体 
+
+
+        libxml_disable_entity_loader(true);
+
+
+        $xmlstring = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+
+        $val = json_decode(json_encode($xmlstring), true);
+
+
+        return $val;
+    }
+
+    private static function postXmlCurl($xml, $url, $second = 30) 
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); //严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+
+
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 40);
+        set_time_limit(0);
+
+
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if ($data) {
+            curl_close($ch);
+            return $data;
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+            throw new WxPayException("curl出错，错误码:$error");
+        }
+    }
+
+    public function createNoncestr($length = 32) {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
     }
 
     public function actionPay()
