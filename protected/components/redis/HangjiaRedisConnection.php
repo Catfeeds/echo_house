64a99bf8ee5440fc1ab1beccdc4739102d04a43c
ztenst <754710007@qq.com>
2018-04-17 16:54:50 +0800

    yohua

--- protected/components/redis/HangjiaRedisConnection.php
+++ protected/components/redis/HangjiaRedisConnection.php
@@ -19,7 +19,7 @@ class HangjiaRedisConnection extends CApplicationComponent
      * redis连接地址
      * @var string
      */
-    public $hostname = '127.0.0.1';
+    public $hostname = '120.77.44.227';
     /**
      * redis端口号
      * @var string
