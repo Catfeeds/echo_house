8e49be8f57c3418dab0011c91bc11b3fe86e562f
ztenst <754710007@qq.com>
2018-05-06 17:01:46 +0800

    '城市'

--- subwap/js/personallist.js
+++ subwap/js/personallist.js
@@ -114,6 +114,30 @@ myapp.controller('perlistCtrl',function($scope,$http) {
         location.href='houseRefresh.html?num=' + refreshNum;     
     }
 
+    // 上架
+    $scope.addHouse=function(e,id){
+        e.stopPropagation();
+        e.preventDefault();
+        reason=reason==''?$('.tip-off-detail').val():reason;
+        $http({
+            method: 'GET',
+            url: '/api/plot/downPlot',
+            params:{
+                'type':1,
+                'hid':id,
+                'note':'上架'
+            }
+        }).then(function successCallback(response) {
+            if (response.data.status=='error') {
+                alert(response.data.msg);
+            }else{
+                alert('申请上架成功，等待后台人员审核!');
+            }
+
+        });
+     
+    }
+
     // 下架弹框
     $scope.showClose=function(e,id){
         e.stopPropagation();
