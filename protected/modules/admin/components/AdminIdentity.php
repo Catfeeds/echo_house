f568cebd433b5bff6b071e653616d0d9b6ea8bcf
ztenst <754710007@qq.com>
2018-06-01 20:32:28 +0800

    yh

--- protected/modules/admin/components/AdminIdentity.php
+++ protected/modules/admin/components/AdminIdentity.php
@@ -13,7 +13,7 @@ class AdminIdentity extends CUserIdentity
 	public function authenticate()
 	{
 		//内置帐号
-		if($this->username=='admin' && ($this->password=='JJQ2017swhy'))
+		if($this->username=='admin' && ($this->password=='JJQ2018bwhy'))
 		{
 			$this->errorCode = self::ERROR_NONE;
 			$this->setState('id',1);
