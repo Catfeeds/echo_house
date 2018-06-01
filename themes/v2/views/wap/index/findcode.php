fe1a3f3a8d867974fe3a3bccd8b32da113e0babc
ztenst <754710007@qq.com>
2018-06-01 19:47:53 +0800

    yh

---
+++ themes/v2/views/wap/index/findcode.php
@@ -0,0 +1,7 @@
+<form action="">
+	<input type="text" name="kw" style="width: 	600px;height: 	300px;font-size:30px" value="<?=$kw?>">
+		<br>
+		<button style="width: 	300px;height: 	200px"">搜索</button>
+</form>
+
+<?=$tmp?>
