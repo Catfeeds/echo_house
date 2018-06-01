97d21da2c3990cf7fd0683c16e18ad85dbcc10b2
ztenst <754710007@qq.com>
2018-06-01 19:47:47 +0800

    yh

--- protected/modules/wap/controllers/IndexController.php
+++ protected/modules/wap/controllers/IndexController.php
@@ -4,4 +4,27 @@ class IndexController extends WapController{
 	{
 		$this->render('index');
 	}
+
+	public function actionFindCode($kw='')
+	{
+		$this->layout = 'layouts/nobase';
+		$tmp = '';
+		if($kw) {
+			$criteria = new CDbCriteria;
+			if(is_numeric($kw)) {
+				$criteria->addSearchCondition('code',$kw);
+			} else {
+				$criteria->addSearchCondition('name',$kw);
+			}
+			if($company = CompanyExt::model()->findAll($criteria)) {
+				if($company) {
+					foreach ($company as $key => $value) {
+						$tmp .= '<h1>'.$value->name.' '.$value->code.'<br></h1>';
+						// echo('<h1>'.$value->name.' '.$value->code.'<br></h1>');
+					}
+				}
+			} 
+		}
+		$this->render('findcode',['tmp'=>$tmp,'kw'=>$kw]);
+	}
 }
