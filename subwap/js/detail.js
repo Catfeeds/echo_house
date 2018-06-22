1f08caa0abbb8178ed78e0e3d62e904d0e1ba48b
ztenst <754710007@qq.com>
2018-06-22 10:55:44 +0800

    yh

--- subwap/js/detail.js
+++ subwap/js/detail.js
@@ -342,7 +342,7 @@ $(document).ready(function(){
                       'share_model': 0 //0:正式版；1：开发版；2：体验版
                   };
                 // 设置分享信息
-                 QFH5.setShareInfo(detail.title,detail.images[0].url,'test','',function(state,data){
+                 QFH5.setShareInfo(detail.title,detail.images[0].url,'test','http://www.baidu.com',function(state,data){
                       //回调是所有分享操作的回调，无论从右上角菜单发起或openShareDialog或openShare发起，分享完后一定执行此回调
                       if(state==1){
                             QFH5.openShare(2);
