fe8e13857ce1fc15ee7e24c626219a4dbff89ba0
ztenst <754710007@qq.com>
2018-07-16 01:45:19 +0800

    yh

--- protected/modules/api/controllers/PlotController.php
+++ protected/modules/api/controllers/PlotController.php
@@ -347,7 +347,7 @@ class PlotController extends ApiController{
 			}
 		}
 		if($uid) {
-			$this->frame['data']['fresh_num'] = Yii::app()->db->createCommand("select refresh_num from user where id=$uid")->queryScalar();
+			$this->frame['data']['fresh_num'] = $this->staff->refresh_num;
 		}
 		if($city+$area+$street+$aveprice+$sfprice+$wylx+$zxzt+$toptag+$company+$uid+$save==0&&!$kw) {
 			$this->frame['data']['num'] += 800;
